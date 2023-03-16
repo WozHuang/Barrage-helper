73bdd79610530780448a3f1bd703e31f240fa679
WozHuang <wozhuang96@gmail.com>
2023-03-16 21:30:46 +0800

    build: 打包 server

--- config/webpack.server.config.ts
+++ config/webpack.server.config.ts
@@ -1,16 +1,49 @@
 import path from 'node:path';
 import { Configuration } from 'webpack';
 import { rules } from './webpack.rules';
+// import TerserPlugin from 'terser-webpack-plugin';
+import { WebpackManifestPlugin } from 'webpack-manifest-plugin';
+import nodeExternals from 'webpack-node-externals';
 
 const config: Configuration = {
   mode: 'production',
-  // devtool: 'eval-source-map',
+  devtool: 'source-map',
   target: 'node',
-  entry: './src/server/index.ts',
+  entry: {
+    index: './src/server/index.ts'
+  },
   output: {
     path: path.resolve(__dirname, '../out/server'),
-    filename: 'index.js',
+    filename: '[name].js',
+    clean: true
     // devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]',
+  },
+  optimization: {
+    // 隐藏所有注释，包括 License 声明
+    // minimize: true,
+    // minimizer: [
+    //   new TerserPlugin({
+    //     terserOptions: {
+    //       format: {
+    //         comments: false,
+    //       },
+    //     },
+    //     extractComments: false,
+    //   })
+    // ],
+
+    // 将 node_modules 依赖分割到单独文件中，但如果指定 externals 忽略了node_modules 就没必要了
+    splitChunks: {
+      chunks: 'initial',
+      cacheGroups: {
+        vendors: {
+          name: 'chunk-vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          chunks: 'initial'
+        },
+      }
+    }
   },
   module: {
     rules,
@@ -18,6 +51,21 @@ const config: Configuration = {
   resolve: {
     extensions: ['.js', '.ts', '.json'],
   },
+  plugins: [
+    // 输出 manifest.json 以便索引文件
+    // new WebpackManifestPlugin({})
+  ],
+
+  // @link https://webpack.docschina.org/configuration/externals/
+  // 不打包依赖
+  // externals: [
+  //   /^koa.*/i,
+  //   'dotenv'
+  // ],
+  // 还可以考虑通过 webpack-node-externals 插件实现
+  externals: [nodeExternals()],
+  // 指定外部依赖引入方式
+  externalsType: 'node-commonjs',
 };
 
 export default config;
